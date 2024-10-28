a7228b3f359c4caeac3f1cfde9ef367695395a35
William Lawrence <lawrence.william86@gmail.com>
2024-10-28 15:03:50 -0400

    made tornado pic on home page visible with import

--- src/containers/hero-homePage/Hero-HomePage.jsx
+++ src/containers/hero-homePage/Hero-HomePage.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './hero-homePage.css';
+import { studentWork } from '../../../public';
 
 const HeroHomepage = (props) => {
 	return (
@@ -10,7 +11,7 @@ const HeroHomepage = (props) => {
 			<div id='homeHeader' className='container text-center my-5'>
 				<div className='row align-items-center'>
 					<div className='col-md-6'>
-						<img className='headerImg' src='../public/images/websiteImages/goldenTornado.webp' />
+						<img className='headerImg' src={studentWork} />
 					</div>
 					<div className='col-md-6'>
 						<p>{props.teacher}</p>
